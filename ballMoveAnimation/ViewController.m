f9426eb24421b7bc587f518ece6d46ec93b4e15a
jungeun826 <jungeun826@nate.com>
2014-01-08 12:51:22 +0900

    238~239 실습

--- ballMoveAnimation/ViewController.m
+++ ballMoveAnimation/ViewController.m
@@ -8,16 +8,47 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    BOOL holdingImage;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *ballImage;
 
 @end
 
 @implementation ViewController
+-(void)touchesBegan : (NSSet *)touches withEvent:(UIEvent *)event{
+    
+    UITouch *touch = [touches anyObject];
+    CGPoint Point = [touch locationInView:self.view];
+    
+    if(CGRectContainsPoint (self.ballImage.frame,Point)){
+        holdingImage = YES;
+        self.ballImage.transform = CGAffineTransformMakeScale(1.1,1.1);
+    }
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    NSLog(@"moved count:%d",(int)[[event allTouches]count]);
+    
+    if(holdingImage){
+        UITouch *touch = [ touches anyObject];
+        CGPoint point = [touch locationInView:self.view];
+        self.ballImage.center = point;
+    }
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+    if(holdingImage){
+        self.ballImage.transform = CGAffineTransformIdentity;
+        holdingImage = NO;
+    }
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+
 }
 
 - (void)didReceiveMemoryWarning
